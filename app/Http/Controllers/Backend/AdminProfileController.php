b60bf20464a638bb7d2bef0254139edfd26f4550
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-02-14 14:56:59 +0200

    update

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -8,91 +8,96 @@ use App\Models\Admin;
 use App\Models\User;
 use Auth;
 use Illuminate\Support\Facades\Hash;
- 
+
 class AdminProfileController extends Controller
-{ 
-   
-	public function AdminProfile(){
+{
 
-		$id = Auth::user()->id;
-		$adminData = Admin::find($id);
-		return view('admin.admin_profile_view',compact('adminData'));
-	}
- 
+    public function AdminProfile()
+    {
 
-	public function AdminProfileEdit(){
-
-		$id = Auth::user()->id;
-		$editData = Admin::find($id);
-		return view('admin.admin_profile_edit',compact('editData'));
-
-	}
+        $id = Auth::user()->id;
+        $adminData = Admin::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }
 
 
-	public function AdminProfileStore(Request $request){
+    public function AdminProfileEdit()
+    {
 
-		$id = Auth::user()->id;
-		$data = Admin::find($id);
-		$data->name = $request->name;
-		$data->email = $request->email;
+        $id = Auth::user()->id;
+        $editData = Admin::find($id);
+        return view('admin.admin_profile_edit', compact('editData'));
 
-
-		if ($request->file('profile_photo_path')) {
-			$file = $request->file('profile_photo_path');
-			@unlink(public_path('upload/admin_images/'.$data->profile_photo_path));
-			$filename = date('YmdHi').$file->getClientOriginalName();
-			$file->move(public_path('upload/admin_images'),$filename);
-			$data['profile_photo_path'] = $filename;
-		}
-		$data->save();
-
-		$notification = array(
-			'message' => 'Admin Profile Updated Successfully',
-			'alert-type' => 'success'
-		);
-
-		return redirect()->route('admin.profile')->with($notification);
-
-	} // end method 
+    }
 
 
+    public function AdminProfileStore(Request $request)
+    {
 
-	public function AdminChangePassword(){
+        $id = Auth::user()->id;
+        $data = Admin::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
 
-		return view('admin.admin_change_password');
+        if ($request->hasFile('profile_photo_path')) {
+            auth()->user()->addMedia($request->file('profile_photo_path'))->toMediaCollection('avatars');
+        }
 
-	}
+//        if ($request->file('profile_photo_path')) {
+//            $file = $request->file('profile_photo_path');
+//            @unlink(public_path('upload/admin_images/' . $data->profile_photo_path));
+//            $filename = date('YmdHi') . $file->getClientOriginalName();
+//            $file->move(public_path('upload/admin_images'), $filename);
+//            $data['profile_photo_path'] = $filename;
+//        }
+        $data->save();
 
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
 
-	public function AdminUpdateChangePassword(Request $request){
+        return redirect()->route('admin.profile')->with($notification);
 
-		$validateData = $request->validate([
-			'oldpassword' => 'required',
-			'password' => 'required|confirmed',
-		]);
-
-		$hashedPassword = Auth::user()->password;
-		if (Hash::check($request->oldpassword,$hashedPassword)) {
-			$admin = Admin::find(Auth::id());
-			$admin->password = Hash::make($request->password);
-			$admin->save();
-			Auth::logout();
-			return redirect()->route('admin.logout');
-		}else{
-			return redirect()->back();
-		}
-
-
-	}// end method
-
-
-
-	public function AllUsers(){
-		$users = User::latest()->get();
-		return view('backend.user.all_user',compact('users'));
-	}
+    } // end method
 
 
+    public function AdminChangePassword()
+    {
+
+        return view('admin.admin_change_password');
+
+    }
+
+
+    public function AdminUpdateChangePassword(Request $request)
+    {
+
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword, $hashedPassword)) {
+            $admin = Admin::find(Auth::id());
+            $admin->password = Hash::make($request->password);
+            $admin->save();
+            Auth::logout();
+            return redirect()->route('admin.logout');
+        } else {
+            return redirect()->back();
+        }
+
+
+    }// end method
+
+
+    public function AllUsers()
+    {
+        $users = User::latest()->get();
+        return view('backend.user.all_user', compact('users'));
+    }
 
 
 }
--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -9,14 +9,18 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements HasMedia
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
@@ -54,4 +58,28 @@ class Admin extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    //This is The method that does all the magic
+
+    public function registerMediaCollections(): void
+    {
+        // TODO: Implement registerMediaCollections() method.
+        $this->addMediaCollection('avatars')
+            ->registerMediaConversions(function (Media $media) {
+                $this->addMediaConversion('profile image')
+                    ->height(100)
+                    ->width(100)
+                    ->sharpen(10);
+            });
+    }
+
+    public function getImagesAttribute()
+    {
+        if (!empty($this->getFirstMediaUrl('avatars','profile image'))) {
+            return $this->getFirstMediaUrl('avatars','profile image');
+        }
+        return asset('upload/no_image.jpg');
+    }
+
+
 }
